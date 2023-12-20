e31cf6c87f7907f89e33fbfb12807f1d6f88583e
johnma2006 <johnma2006@gmail.com>
2023-12-20 11:15:24 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,32 @@
+## mamba-minimal
+
+Simple, minimal implementation of Mamba in one file of PyTorch.
+
+Featuring:
+* Equivalent numerical output as official implementation for both forward and backward pass
+* Simplified, readable, annotated code
+
+Does NOT include:
+* Speed. The official implementation is heavily optimized, and these optimizations are core contributions of the Mamba paper. I kept most implementations simple for readability.
+* Proper parameter initialization (though this could be added without sacrificing readability)
+
+## Demo
+
+See [demo.ipynb](demo.ipynb) for examples of prompt completions.
+
+```python
+from model import Mamba, ModelArgs
+from transformers import AutoTokenizer
+
+model = Mamba.from_pretrained('state-spaces/mamba-370m')
+tokenizer = AutoTokenizer.from_pretrained('EleutherAI/gpt-neox-20b')
+
+generate(model, tokenizer, 'Mamba is the')
+```
+> Mamba is the world's longest venomous snake with an estimated length of over 150 m. With such a large size and a venomous bite, Mamba kills by stabbing the victim (which is more painful and less effective than a single stab of the bite)
+
+## References
+
+The Mamba architecture was introduced in [Mamba: Linear-Time Sequence Modeling with Selective State Spaces](https://arxiv.org/abs/2312.00752) by [Albert Gu](https://twitter.com/_albertgu?lang=en) and [Tri Dao](https://twitter.com/tri_dao?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor).
+
+The official implementation is here: https://github.com/state-spaces/mamba/tree/main
---
+++ model.py
@@ -0,0 +1,314 @@
+"""Simple, minimal implementation of Mamba in one file of PyTorch.
+
+Suggest reading the following before/while reading the code:
+    [1] Mamba: Linear-Time Sequence Modeling with Selective State Spaces (Albert Gu and Tri Dao): https://arxiv.org/abs/2312.00752
+    [2] The Annotated S4 (Sasha Rush and Sidd Karamcheti): https://srush.github.io/annotated-s4
+
+References:
+    * Official Mamba implementation: https://github.com/state-spaces/mamba/tree/main
+
+Glossary:
+    b: batch size
+    l: sequence length
+    d (AKA d_model): hidden dim
+    n (AKA d_state): latent state dim (this is `N` in Mamba paper [1] Algorithm 2)
+    expand: expansion factor (see Mamba paper [1] Section 3.4)
+    d_in (AKA d_inner): d * expand
+    dt_rank: rank of delta Δ (see Mamba paper [1] Section 3.6 "Parameterization of ∆")
+
+"""
+from __future__ import annotations
+import math
+import json
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from dataclasses import dataclass
+from einops import rearrange, repeat, einsum
+
+
+@dataclass
+class ModelArgs:
+    d_model: int
+    n_layer: int
+    vocab_size: int
+    d_state: int = 16
+    expand: int = 2
+    dt_rank: Union[int, str] = 'auto'
+    d_conv: int = 4 
+    pad_vocab_size_multiple: int = 8
+    conv_bias: bool = True
+    bias: bool = False
+    
+    def __post_init__(self):
+        self.d_inner = int(self.expand * self.d_model)
+        
+        if self.dt_rank == 'auto':
+            self.dt_rank = math.ceil(self.d_model / 16)
+            
+        if self.vocab_size % self.pad_vocab_size_multiple != 0:
+            self.vocab_size += self.pad_vocab_size_multiple - self.vocab_size % self.pad_vocab_size_multiple
+
+
+class Mamba(nn.Module):
+    def __init__(self, args: ModelArgs):
+        """Full Mamba model."""
+        super().__init__()
+        self.args = args
+        self.embedding = nn.Embedding(args.vocab_size, args.d_model)
+        self.layers = nn.ModuleList([ResidualBlock(args) for _ in range(args.n_layer)])
+        self.norm_f = RMSNorm(args.d_model)
+
+        self.lm_head = nn.Linear(args.d_model, args.vocab_size, bias=False)
+        self.lm_head.weight = self.embedding.weight  # Tie output projection to embedding weights. See "Weight Tying" paper
+
+
+    def forward(self, input_ids):
+        """
+        Args:
+            input_ids (long tensor): shape (b, l)    (See Glossary at top for definitions of b, l, d_in, n...)
+    
+        Returns:
+            logits: shape (b, l, d)
+
+        Official Implementation:
+            class MambaLMHeadModel, https://github.com/state-spaces/mamba/blob/main/mamba_ssm/models/mixer_seq_simple.py#L173
+
+        """
+        x = self.embedding(input_ids)
+        
+        for layer in self.layers:
+            x = layer(x)
+            
+        x = self.norm_f(x)
+        logits = self.lm_head(x)
+
+        return logits
+
+    
+    @staticmethod
+    def from_pretrained(pretrained_model_name: str):
+        """Load model with pretrained weights.
+    
+        Args:
+            pretrained_model_name: One of
+                * 'state-spaces/mamba-2.8b-slimpj'
+                * 'state-spaces/mamba-2.8b'
+                * 'state-spaces/mamba-1.4b'
+                * 'state-spaces/mamba-790m'
+                * 'state-spaces/mamba-370m'
+                * 'state-spaces/mamba-130m'
+                            
+        Returns:
+            model: Mamba model with weights loaded
+    
+        """
+        from transformers.utils import WEIGHTS_NAME, CONFIG_NAME
+        from transformers.utils.hub import cached_file
+        
+        def load_config_hf(model_name):
+            resolved_archive_file = cached_file(model_name, CONFIG_NAME, _raise_exceptions_for_missing_entries=False)
+            return json.load(open(resolved_archive_file))
+        
+        
+        def load_state_dict_hf(model_name, device=None, dtype=None):
+            resolved_archive_file = cached_file(model_name, WEIGHTS_NAME, _raise_exceptions_for_missing_entries=False)
+            return torch.load(resolved_archive_file)
+        
+        config_data = load_config_hf(pretrained_model_name)
+        args = ModelArgs(
+            d_model=config_data['d_model'],
+            n_layer=config_data['n_layer'],
+            vocab_size=config_data['vocab_size']
+        )
+        model = Mamba(args)
+        
+        state_dict = load_state_dict_hf(pretrained_model_name)
+        new_state_dict = {}
+        for key in state_dict:
+            new_key = key.replace('backbone.', '')
+            new_state_dict[new_key] = state_dict[key]
+        model.load_state_dict(new_state_dict)
+        
+        return model
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, args: ModelArgs):
+        """Simple block wrapping Mamba block with normalization and residual connection."""
+        super().__init__()
+        self.args = args
+        self.mixer = MambaBlock(args)
+        self.norm = RMSNorm(args.d_model)
+        
+
+    def forward(self, x):
+        """
+        Args:
+            x: shape (b, l, d)    (See Glossary at top for definitions of b, l, d_in, n...)
+    
+        Returns:
+            output: shape (b, l, d)
+
+        Official Implementation:
+            Block.forward(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/modules/mamba_simple.py#L297
+            NOTE: the official repo does "Add -> Norm -> Mamba, returning both", purely for performance reasons as this
+            allows them to do fused add + norm. We revert back to "LN -> Mamba -> Add" as it is simpler.
+            
+        """
+        output = self.mixer(self.norm(x)) + x
+
+        return output
+            
+
+class MambaBlock(nn.Module):
+    def __init__(self, args: ModelArgs):
+        """A single Mamba block, as described in Figure 3 in Section 3.4 in the Mamba paper [1]."""
+        super().__init__()
+        self.args = args
+
+        self.in_proj = nn.Linear(args.d_model, args.d_inner * 2, bias=args.bias)
+
+        self.conv1d = nn.Conv1d(
+            in_channels=args.d_inner,
+            out_channels=args.d_inner,
+            bias=args.conv_bias,
+            kernel_size=args.d_conv,
+            groups=args.d_inner,
+            padding=args.d_conv - 1,
+        )
+
+        # x_proj takes in `x` and outputs the input-specific Δ, B, C
+        self.x_proj = nn.Linear(args.d_inner, args.dt_rank + args.d_state * 2, bias=False)
+        
+        # dt_proj projects Δ from dt_rank to d_in
+        self.dt_proj = nn.Linear(args.dt_rank, args.d_inner, bias=True)
+
+        A = repeat(torch.arange(1, args.d_state + 1), 'n -> d n', d=args.d_inner)
+        self.A_log = nn.Parameter(torch.log(A))
+        self.D = nn.Parameter(torch.ones(args.d_inner))
+        self.out_proj = nn.Linear(args.d_inner, args.d_model, bias=args.bias)
+        
+
+    def forward(self, x):
+        """Mamba block forward. This looks the same as Figure 3 in Section 3.4 in the Mamba paper [1].
+    
+        Args:
+            x: shape (b, l, d)    (See Glossary at top for definitions of b, l, d_in, n...)
+    
+        Returns:
+            output: shape (b, l, d)
+
+        
+        Official Implementation:
+            class Mamba, https://github.com/state-spaces/mamba/blob/main/mamba_ssm/modules/mamba_simple.py#L119
+            
+        """
+        (b, l, d) = x.shape
+        
+        x_and_res = self.in_proj(x)  # shape (b, l, 2 * d_in)
+        x_and_res = rearrange(x_and_res, 'b l x -> b x l')
+        (x, res) = x_and_res.split(split_size=[self.args.d_inner, self.args.d_inner], dim=1)
+        
+        x = self.conv1d(x)[:, :, :l]
+        x = F.silu(x)
+
+        y = self.ssm(x)
+        
+        y = y * F.silu(res)
+        
+        output = self.out_proj(rearrange(y, 'b d_model l -> b l d_model'))
+
+        return output
+
+    
+    def ssm(self, x):
+        """Runs the SSM. See:
+            - Algorithm 2 in Section 3.2 in the Mamba paper [1]
+            - run_SSM(A, B, C, u) in The Annotated S4 [2]
+    
+        Args:
+            x: shape (b, d_in, l)    (See Glossary at top for definitions of b, l, d_in, n...)
+    
+        Returns:
+            output: shape (b, d_in, l)
+
+        Official Implementation:
+            mamba_inner_ref(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/ops/selective_scan_interface.py#L311
+            
+        """
+        (d_in, n) = self.A_log.shape
+
+        A = -torch.exp(self.A_log.float())  # shape (d_in, n)
+
+        # As opposed to S4 which have time invariant A B C, in Mamba we let ∆ B C be functions of the input
+        x_dbl = rearrange(x, 'b d l -> b l d')
+        x_dbl = self.x_proj(x_dbl)  # (b, l, dt_rank + 2*n)
+        
+        (delta, B, C) = x_dbl.split(split_size=[self.args.dt_rank, n, n], dim=-1)  # delta: (b, l, dt_rank). B, C: (b, l, n)
+        delta = F.softplus(self.dt_proj(delta))  # (b, l, d_in)
+        D = self.D.float()
+        
+        y = self.selective_scan(x, delta, A, B, C, D)  # This is similar to run_SSM(A, B, C, u) in The Annotated S4 [2]
+        
+        return y
+
+    
+    def selective_scan(self, u, delta, A, B, C, D):
+        """Does selective scan algorithm. See:
+            - Algorithm 2 in Section 3.2 in the Mamba paper [1]
+            - run_SSM(A, B, C, u) in The Annotated S4 [2]
+    
+        Args:
+            u: shape (b, d_in, l)    (See Glossary at top for definitions of b, l, d_in, n...)
+            delta: shape (b, l, d_in)
+            A: shape (d_in, n)
+            B: shape (b, l, n)
+            C: shape (b, l, n)
+            D: shape (d_in,)
+    
+        Returns:
+            output: shape (b, d_in, l)
+    
+        Official Implementation:
+            selective_scan_ref(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/ops/selective_scan_interface.py#L86
+            Note: I refactored some parts out of `selective_scan_ref` out, so the functionality doesn't match exactly.
+            
+        """
+        (b, d_in, l) = u.shape
+        n = A.shape[1]
+        
+        # Discretize continuous parameters (Δ, A, B)  (see Section 2 Equation 4 in the Mamba paper [1])
+        # Note that B is parameterized directly
+        deltaA = torch.exp(einsum(delta, A, 'b l d_in, d_in n -> b d_in l n'))
+        deltaB_u = einsum(delta, B, u, 'b l d_in, b l n, b d_in l -> b d_in l n')
+        
+        # Perform selective scan (see scan_SSM() in The Annotated S4 [2])
+        x = torch.zeros((b, d_in, n))
+        ys = []    
+        for i in range(l):
+            x = deltaA[:, :, i] * x + deltaB_u[:, :, i]
+            y = einsum(x, C[:, i, :], 'b d_in n , b n -> b d_in')
+            ys.append(y)
+        y = torch.stack(ys, dim=2) # (b d_in l)
+        
+        if D is not None:
+            y = y + u * rearrange(D, 'd_in -> d_in 1')
+    
+        return y
+
+
+class RMSNorm(nn.Module):
+    def __init__(self,
+                 d_model: int,
+                 eps: float = 1e-5):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(d_model))
+
+
+    def forward(self, x):
+        output = x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps) * self.weight
+
+        return output
+        
