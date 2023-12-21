117224c0b0864d563f72eb7370ac592e5721ea81
johnma2006 <johnma2006@gmail.com>
2023-12-21 17:19:04 +0000

    Update some documentation about ZOH / euler discretization

--- model.py
+++ model.py
@@ -9,13 +9,13 @@ Suggest reading the following before/while reading the code:
 Glossary:
     b: batch size                       (`B` in Mamba paper [1] Algorithm 2)
     l: sequence length                  (`L` in [1] Algorithm 2)
-    d (AKA d_model): hidden dim
-    n (AKA d_state): latent state dim   (`N` in [1] Algorithm 2)
-    expand: expansion factor            (See Mamba paper [1] Section 3.4)
-    d_in (AKA d_inner): d * expand      (`D` in [1] Algorithm 2)
+    d or d_model: hidden dim
+    n or d_state: latent state dim      (`N` in [1] Algorithm 2)
+    expand: expansion factor            (`E` in [1] Section 3.4)
+    d_in or d_inner: d * expand         (`D` in [1] Algorithm 2)
     A, B, C, D: state space parameters  (See any state space representation formula)
-                                        (B, C are input-dependent; A, D are not)
-    Δ (AKA delta): input-dependent step size
+                                        (B, C are input-dependent (aka selective, a key innovation in Mamba); A, D are not)
+    Δ or delta: input-dependent step size
     dt_rank: rank of Δ                  (See [1] Section 3.6 "Parameterization of ∆")
 
 """
@@ -302,8 +302,10 @@ class MambaBlock(nn.Module):
         (b, l, d_in) = u.shape
         n = A.shape[1]
         
-        # Discretize continuous parameters (Δ, A, B) using zero-order hold (ZOH) discretization
-        # See Section 2 Equation 4 in the Mamba paper [1]
+        # Discretize continuous parameters (A, B)
+        # - A is discretized using zero-order hold (ZOH) discretization (see Section 2 Equation 4 in the Mamba paper [1])
+        # - B is discretized using a simplified Euler discretization instead of ZOH. From a discussion with authors:
+        #   "A is the more important term and the performance doesn't change much with the simplication on B"
         deltaA = torch.exp(einsum(delta, A, 'b l d_in, d_in n -> b d_in l n'))
         deltaB_u = einsum(delta, B, u, 'b l d_in, b l n, b l d_in -> b d_in l n')
         
@@ -314,7 +316,7 @@ class MambaBlock(nn.Module):
             x = deltaA[:, :, i] * x + deltaB_u[:, :, i]
             y = einsum(x, C[:, i, :], 'b d_in n, b n -> b d_in')
             ys.append(y)
-        y = torch.stack(ys, dim=1)  # (b, l, d_in)
+        y = torch.stack(ys, dim=1)  # shape (b, l, d_in)
         
         y = y + u * D
     
